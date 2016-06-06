534e48756778e36e95fca27bc2b55f34f06ecda1
SvetlanaM <svetlana@margetova.eu>
2016-06-06 12:14:27 +0200

    initial-commit

---
+++ czechitas-mobile-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  czechitas-mobile-app
+//
+//  Created by Svetlana Margetová on 06.06.16.
+//  Copyright © 2016 Svetlana Margetová. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
